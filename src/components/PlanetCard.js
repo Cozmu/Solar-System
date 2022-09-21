803c30b90a2284e5f6197d764a5ea336a7ad8d04
Cozmu <www.jorgereis@gmail.com>
2022-09-21 00:53:43 -0300

    Adicionando requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Planets from '../data/planets';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+export default PlanetCard;
