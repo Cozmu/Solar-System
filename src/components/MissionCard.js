691201ccf2b49cbd235c89d161aeeaa9920ac334
Cozmu <www.jorgereis@gmail.com>
2022-09-21 02:37:03 -0300

    Adicionando requisito 9(sem lint)

---
+++ src/components/MissionCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{name}</p>
+        <p data-testid="mission-year">{year}</p>
+        <p data-testid="mission-country">{country}</p>
+        <p data-testid="mission-destination">{destination}</p>
+      </div>
+    );
+  }
+}
+
+export default MissionCard;
