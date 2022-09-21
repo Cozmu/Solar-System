fd5efa139c8beeec27bce187ab0e0e9428fbded6
Cozmu <www.jorgereis@gmail.com>
2022-09-20 23:24:04 -0300

    Adicionando requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 
 export default App;
+// <p>Sistema Solar</p>
